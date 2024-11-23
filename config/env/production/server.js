ea01b0882a7dec26c9edcc75567cb24998e07c54
Denzel Co <denzel_adrian_co@dlsu.edu.ph>
2024-11-23 19:15:05 +0800

    added render yaml and removed heroku configuraiton

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,6 @@
-// Path: ./config/env/production/server.js`
-export default ({ env }) => ({
-  proxy: true,
-  url: env("MY_HEROKU_URL"), // Sets the public URL of the application.
-  app: {
-    keys: env.array("APP_KEYS"),
+module.exports = ({ env }) => ({
+  url: env("RENDER_EXTERNAL_URL"),
+  dirs: {
+    public: "/data/public",
   },
 });
